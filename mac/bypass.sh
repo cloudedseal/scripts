2419492cde1342731fcd38f39ce5a6db5ee73fe0
cloudedseal <2652207782@qq.com>
2025-07-24 10:38:23 +0800

    macos network bypassdomain config

--- mac/bypass.sh
+++ mac/bypass.sh
@@ -6,10 +6,11 @@
 # Script to add a domain to the proxy bypass list for a specific network interface on macOS
 
 NIC="AX88179A"
+DOMAIN="*.zhixuan.com"
 echo "Current proxy bypass domains for $NIC:"
 networksetup -getproxybypassdomains $NIC
 
-sudo networksetup -getproxybypassdomains $NIC  | grep -x '.zhixuan.com' > /dev/null || sudo networksetup -setproxybypassdomains $NIC "$(sudo networksetup -getproxybypassdomains $NIC | tr '\n' ','  | xargs) *.zhixuan.com"
+networksetup -getproxybypassdomains $NIC  | grep -x "$DOMAIN" > /dev/null || sudo networksetup -setproxybypassdomains $NIC "$(sudo networksetup -getproxybypassdomains $NIC | tr '\n' ','  | xargs) $DOMAIN"
 echo "Updated proxy bypass domains for $NIC"
 
 networksetup -getproxybypassdomains $NIC 
